d68f22545b596c0ef5420f50748202465afe36d4
gasparnd <gaspardolcemascolo@gmail.com>
2023-12-16 23:58:38 -0300

    Initial Commit

---
+++ Platzi/PlatziApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziApp.swift
+//  Platzi
+//
+//  Created by Gaspar Dolcemascolo on 16/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlatziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
